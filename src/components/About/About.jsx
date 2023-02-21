3928895b6f2ff3eaa7085f6ae4fc25b117a21919
Kary1661 <posposkarina@gmail.com>
2023-02-21 13:51:14 -0300

    'Last-Changes'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return(
         <div className={style.AboutContainer}>
             <h1>Esta es la app de Kary</h1>
-            <h2>Cohorte: FT-34b</h2>
+            <h3>Cohorte: FT-35b</h3>
         </div>
     )
 }
