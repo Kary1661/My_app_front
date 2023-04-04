7e236f0f75a1aebf45eca66d9cc1c2f52c713b5d
Kary1661 <posposkarina@gmail.com>
2023-04-04 13:20:44 -0300

    'last'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import * as ReactDOMClient from "react-dom/client";
 import "./index.modules.css";
 import App from "./App";
+import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import store from "./components/Redux/store";
 import { Provider } from "react-redux";
 
+
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
